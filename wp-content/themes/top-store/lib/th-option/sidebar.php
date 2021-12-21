148e648209fdd852249eff36c8f2284331311905
tungbuikbhb <tunghb452001@gmail.con>
2021-12-21 12:30:31 +0700

    upload

---
+++ wp-content/themes/top-store/lib/th-option/sidebar.php
@@ -0,0 +1,32 @@
+<div class="sidebar-section">
+            <div class="section">
+            <h3 class="hndle ui-sortable-handle">
+                <span><?php _e('Top Store Pro WordPress Theme','top-store'); ?> </span>
+            </h3>
+            <p>
+            <b><?php _e('14+ pre-made premium templates','top-store'); ?> </b><br>
+            <?php _e('Top Store Pro is one of the most popular single page theme on WordPress.','top-store'); ?> </p>
+                <center><a class="button ztabtn" target="_blank" href="https://themehunk.com/product/top-store-pro/"><?php _e('Upgrade','top-store'); ?> </a></center>
+            </div>
+            <hr>
+            <div class="section">
+                <h3><?php _e('Leave us a review','top-store'); ?></h3>
+                <p><?php _e('We would love to hear your feedback.','top-store'); ?> </p>
+                 <a href="https://wordpress.org/support/theme/top-store/reviews/?rate=5#new-post" target="_blank" class="sidebar-link"><?php _e('Submit review','top-store'); ?></a>
+
+            </div>
+            <hr>
+
+            <div class="section">
+                <h3><?php _e('Video Tutorials','top-store'); ?></h3>
+                <p><?php _e('Want a guide? We have video tutorials to walk you through getting started.','top-store'); ?> </p>
+                <a href="https://www.youtube.com/watch?v=lyYYCNaTjrk&list=PLe42R8TvpeK3NJhL9hDq41M0BrLoUpJRq" target="_blank" class="sidebar-link"><?php _e('Watch Videos','top-store'); ?></a>
+            </div>
+            <hr>
+
+            <div class="section">
+                <h3><?php _e('Support','top-store'); ?> </h3>
+                <p><?php _e('Have a question, we are happy to help! Get in touch with our support team.','top-store'); ?></p>
+                <a href="https://themehunk.com/contact-us/" target="_blank" class="sidebar-link"><?php _e('Submit a Ticket','top-store'); ?></a>
+            </div>
+        </div>
